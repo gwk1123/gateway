ae2a18b9d95dbf3c9e8120d9eb2c7817105c46b0
gwk1123 <g15625281551@163.com>
2022-01-20 23:34:34 +0800

    Gateway 实战负载均衡

--- src/main/java/com/gateway/GatewayApplication.java
+++ src/main/java/com/gateway/GatewayApplication.java
@@ -10,7 +10,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  *
  * http://t.zoukankan.com/caohanren-p-13410534.html
  *
- * https://www.jianshu.com/p/2a2fb061865d  spring cloud gateway报错java.net.UnknownHostException: 4d59d509898a: Name or service not known
+ * spring cloud gateway报错java.net.UnknownHostException: 4d59d509898a: Name or service not known
+ * https://www.jianshu.com/p/2a2fb061865d
  */
 
 @EnableEurekaClient
